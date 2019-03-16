cf87333007e03f9d0723f4d6ca3c232c0002c14c
Angel Ortmann-Li <angel@Valeriyas-MacBook-Air.local>
2019-03-16 15:37:32 -0400

    wrote a merge metho

--- Merge.java
+++ Merge.java
@@ -1,5 +1,13 @@
+import java.util.*;
+import java.io.*;
 public class Merge{
 
+  public static void main(String[] args){
+    int[] a = {1,4,8};
+    int[] b = {3,5,9};
+    int[] array = merge(a,b);
+    System.out.println(Arrays.toString(array));
+  }
   /*sort the array from least to greatest value. This is a wrapper function*/
   public static void mergesort(int[]data){
 
@@ -18,14 +26,12 @@ mergesort(data,lo,hi):
     int a_idx= 0;
     int b_idx= 0;
     for(int i = 0; i < out.length; i++){
-      if(a == a.length);
-      if (a[a_idx]<b[b_idx]){
-        out[i] = a[a_idx++];
-      }else{
-        out[i]=b[b_idx++];
-      }
+      if(a_idx == a.length) out[i]=b[b_idx++];
+      else if (b_idx == b.length) out[i]=a[a_idx++];
+      else if (a[a_idx]<b[b_idx]) out[i] = a[a_idx++];
+      else out[i]=b[b_idx++];
     }
-
+    return out;
   }
 
   private static void mergesortH(int[] og, int a, int b){
