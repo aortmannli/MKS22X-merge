a16a2ec38718260ce4e184de9168e26b904427e2
Angel Ortmann-Li <angel@Valeriyas-MacBook-Air.local>
2019-03-21 18:50:50 -0400

    i cried while i did this and it was all in one sitting

--- Merge.java
+++ Merge.java
@@ -4,38 +4,38 @@ public class Merge{
   public static int insertion = 100;
 
   public static void main(String[]args){
-    System.out.println("Size\t\tMax Value\tmerge/builtin ratio ");
-    int[]MAX_LIST = {1000000000,500,10};
-    for(int MAX : MAX_LIST){
-      for(int size = 31250; size < 2000001; size*=2){
-        long qtime=0;
-        long btime=0;
-        //average of 5 sorts.
-        for(int trial = 0 ; trial <=5; trial++){
-          int []data1 = new int[size];
-          int []data2 = new int[size];
-          for(int i = 0; i < data1.length; i++){
-            data1[i] = (int)(Math.random()*MAX);
-            data2[i] = data1[i];
-          }
-          long t1,t2;
-          t1 = System.currentTimeMillis();
-          Merge.mergesort(data2);
-          t2 = System.currentTimeMillis();
-          qtime += t2 - t1;
-          t1 = System.currentTimeMillis();
-          Arrays.sort(data1);
-          t2 = System.currentTimeMillis();
-          btime+= t2 - t1;
-          if(!Arrays.equals(data1,data2)){
-            System.out.println("FAIL TO SORT!");
-            System.exit(0);
-          }
-        }
-        System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
-      }
-      System.out.println();
-    }
+    System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+   int[]MAX_LIST = {1000000000,500,10};
+   for(int MAX : MAX_LIST){
+     for(int size = 31250; size < 2000001; size*=2){
+       long qtime=0;
+       long btime=0;
+       //average of 5 sorts.
+       for(int trial = 0 ; trial <=5; trial++){
+         int []data1 = new int[size];
+         int []data2 = new int[size];
+         for(int i = 0; i < data1.length; i++){
+           data1[i] = (int)(Math.random()*MAX);
+           data2[i] = data1[i];
+         }
+         long t1,t2;
+         t1 = System.currentTimeMillis();
+         Merge.mergesort(data2);
+         t2 = System.currentTimeMillis();
+         qtime += t2 - t1;
+         t1 = System.currentTimeMillis();
+         Arrays.sort(data1);
+         t2 = System.currentTimeMillis();
+         btime+= t2 - t1;
+         if(!Arrays.equals(data1,data2)){
+           System.out.println("FAIL TO SORT!");
+           System.exit(0);
+         }
+       }
+       System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+     }
+     System.out.println();
+   }
   }
 
 
@@ -82,99 +82,65 @@ public class Merge{
   }
 
   public static void mergesort(int[]data){
-    mergesortOpt(data);
+    mergesortOpt(data, 0, data.length);
   }
 
-  public static void mergesortOpt(int[]data){
-    int[] hold = new int[data.length];
-    mergesortOpt(data, hold, 0, data.length - 1);
-  }
+  private static void mergesortOpt(int[]data, int start, int end){
+      if (start + 1>= end) return;
 
-  private static void mergesortOpt(int[] data, int[] hold, int start, int end){
-    int len = end - start + 1;
-    int pivot = (end + start) / 2;
-    if (len <= 1) return;
-    if (len <= 3){
-      insertionSort(data, start, end);
+      if(end - start < 43){
+        insertionSort(data,start,(start + end + 1)/2);
+        insertionSort(data,(start + end + 1)/2,end);
+      }else{
+        mergesortOpt(data,start,(start + end + 1)/2);
+        mergesortOpt(data,(start + end + 1)/2,end);
+      }
+      merge(data, start, end);
     }
-    else{
-      mergesortOpt(data, hold, start, pivot);
-      mergesortOpt(data, hold, pivot + 1, end);
-      merge(data, hold, start, pivot + 1, end);
-      for (int i = 0; i < len; i++){
-        data[i + start] = hold[i];
-      }
-    }
-  }
 
-  private static void merge(int[] data, int[] hold, int start, int pivot, int end){
-    int i1 = start;
-    int i2 = pivot;
-    int iout = 0;
-    while (i1 < pivot || i2 <= end) {
-      if (i1 >= pivot){
-         hold[iout] = data[i2];
-         i2++;
-       }
-      else if (i2 > end){
-        hold[iout] = data[i1];
-        i1++;
-      }
-      else if (data[i1] > data[i2]){
-        hold[iout] = data[i2];
-        i2++;
-      }
-      else {
-        hold[iout] = data[i1];
-        i1++;
-      }
-      iout++;
-    }
-  }
-
-  private static void mergesort(int[] data, int start, int end){
-      int len = end - start + 1;
-      int pivot = (end + start) / 2;
-      if (len <= 1) return;
-      if (len == 2){
-        if (data[start] > data[end]){
-          swap(data, start, end);
+    private static void merge(int[] data, int start, int end){
+        int x = (end-start + 1)/2;
+        int[] temp1 = new int[x];
+        int[] temp2 = new int[end-start-x];
+        for(int i = 0; i < temp1.length; i++){
+          temp1[i] = data[start+i];
         }
-      }
-      else{
-        int[] d1 = Arrays.copyOfRange(data, start, pivot + 1);
-        int[] d2 = Arrays.copyOfRange(data, pivot + 1, end + 1);
-        mergesort(d1);
-        mergesort(d2);
-        int[] merged = merge(d1, d2);
-        for (int i = 0; i < data.length; i++){
-          data[i] = merged[i];
+        for(int i = 0; i < temp2.length; i++){
+          temp2[i] = data[start+temp1.length + i];
         }
-      }
-    }
 
-
-
-    private static void swap(int[] data, int a, int b){
-      int temp = data[b];
-      data[b] = data[a];
-      data[a] = temp;
-    }
-
-    public static void insertionSort(int[] ary, int lo, int hi){
-      for (int i = lo + 1; i <= hi; i++){
-        int temp = ary[i];
-        if (ary[i - 1] > ary[i]){
-          int x = i - 1;
-          while (x > lo && ary[x - 1] > temp){
-            ary[x + 1] = ary[x];
-            x--;
+        for(int i = start, a = 0, b = 0; i < end; i++){
+          if (a < temp1.length && b < temp2.length){
+            if (temp1[a]>temp2[b]){
+              data[i]=temp2[b];
+              b++;
+            }else{
+              data[i]=temp1[a];
+              a++;
+            }
+          }else{
+            if (a < temp1.length){
+              data[i]=temp1[a];
+              a++;
+            }else {
+              data[i]=temp2[b];
+              b++;
+            }
           }
-          ary[x + 1] = ary[x];
-          ary[x] = temp;
         }
-      }
-  }
+    }
+
+
+    public static void insertionSort(int[] data, int start, int end){
+        int orig;
+        for (int x = 1+start; x < end; x++){
+          orig = data[x];
+          for (int y = x; y > start && data[y] < data[y-1] ; y--){
+              data[y] = data[y-1];
+              data[y-1] = orig;
+          }
+        }
+    }
 
 
 
